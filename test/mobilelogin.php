46f07fa03b10092995f53f47426b2c6b9c0c0214
erolf0123 <50164908+erolf0123@users.noreply.github.com>
2019-07-16 23:20:54 +0900

    update

--- test/mobilelogin.php
+++ test/mobilelogin.php
@@ -8,6 +8,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body class="mobileloginbackground">
-asdf
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <div ><br><br></div>
+                <div class="fontleft font37 greenfont sexyfont">Metting is</div>
+                <div class="fontleft font33 greenfont sexyfont"> &nbsp; Easy</div>
+                <div class="col s3"></div>
+                <div class="col s6 centerfont font37 orangefont">sand</div>
+                <div class="col s3"></div>
+                <br><br><br><br>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix"></i>
+                    <input id="idcheck" name="idcheck" type="text" class="validate">
+                    <label for="icon_prefix">id</label>
+                </div>
+            </div>
+        </div>
+    </div>
 </body> 
 </html>
