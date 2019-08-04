9a7dace0e8e3b2472f4f0db1875a58b671441043
erolf0123 <50164908+erolf0123@users.noreply.github.com>
2019-08-05 02:33:24 +0900

    update

--- app/sandpeople.php
+++ app/sandpeople.php
@@ -1,5 +1,5 @@
 <?php
-	include('connect.php');
+include('connect.php');
 ?>
 <html>
 <head>
@@ -17,13 +17,36 @@
 	<div class="header">
 		<div class="row">
 			<div class="col s3">
-				 <i class="Medium material-icons center fontwhite">arrow_back</i>
+				<i class="Medium material-icons center fontwhite">arrow_back</i>
 			</div>
 			<div class="col s6 center">
 				<div class="fontwhite font22">멤버</div>
 			</div>
 			<div class="col s3">
 				<div class="fontwhite font20 paddingtop20">초대</div>
+			</div>
+		</div>
+	</div>
+	<div class="row backgroundwhite">
+		<div class="col s9">
+			<input value="" id="first_name2" type="text" class="validate"placeholder="글 내용, #태그, @작성자 검색">
+		</div>
+		<div class="col s3">
+			<a class="waves-effect waves-light btn" style="margin-top: 10%">find</a>
+		</div>
+	</div>
+	<div class="optmiddle3">
+		<div class="row">
+			<div class="col s2">
+				<i class="Medium material-icons center paddingtop10">group_add</i>
+			</div>
+			<div class="col s8 center">
+				<div class="paddingtop8 font18black">
+					멤버 초대하기 
+				</div>
+			</div>
+			<div class="col s2">
+				<i class="Medium material-icons center paddingtop10">keyboard_arrow_right</i>
 			</div>
 		</div>
 	</div>
